eba49823e7b08a18ca5c8513c4c9cdbafe9844f2
oe <oe@se.com>
2021-02-22 21:56:15 +0800

    update joy share code

--- scripts/jd_joy_run.js
+++ scripts/jd_joy_run.js
@@ -46,9 +46,9 @@ const $ = new Env('宠汪汪赛跑');
 const JD_BASE_API = `https://draw.jdfcloud.com//pet`;
 //此处填入你需要助力好友的京东用户名
 //下面给出好友邀请助力的示例填写规则
-let invite_pins = ["jd_6cd93e613b0e5,被折叠的记忆33,jd_704a2e5e28a66,jd_45a6b5953b15b,zooooo58,jd_66f5cecc1efcd,jd_41345a6f96aa5,jd_sIhNpDXJehOr,jd_mCbhXxmqzYJC"];
+let invite_pins = ["jd_5dae043c1f610,18601623180_p,tristan111,jd_45a6b5953b15b,zooooo58,jd_66f5cecc1efcd,jd_41345a6f96aa5,jd_sIhNpDXJehOr,jd_mCbhXxmqzYJC"];
 //下面给出好友赛跑助力的示例填写规则
-let run_pins = ["被折叠的记忆33,jd_6cd93e613b0e5,jd_45a6b5953b15b,jd_66f5cecc1efcd,jd_sIhNpDXJehOr,jd_41345a6f96aa5,jd_704a2e5e28a66,zooooo58,jd_5851f32d4a083,jd_mCbhXxmqzYJC,wddpzLSxORvLGo"];
+let run_pins = ["jd_5dae043c1f610,18601623180_p,tristan111,jd_66f5cecc1efcd,jd_sIhNpDXJehOr,jd_41345a6f96aa5,jd_704a2e5e28a66,zooooo58,jd_5851f32d4a083,jd_mCbhXxmqzYJC,wddpzLSxORvLGo"];
 let temp = run_pins[0].split(',')
 let fixPins = temp.splice(temp.indexOf('jd_6cd93e613b0e5'), 1);
 fixPins.push(...temp.splice(temp.indexOf('被折叠的记忆33'), 1));
@@ -57,7 +57,7 @@ temp = [...fixPins, ...randomPins];
 run_pins = [temp.join(',')];
 // $.LKYLToken = '76fe7794c475c18711e3b47185f114b5' || $.getdata('jdJoyRunToken');
 // $.LKYLToken = $.getdata('jdJoyRunToken');
-let friendsArr = ["jd_6cd93e613b0e5", "被折叠的记忆33", "jd_704a2e5e28a66", "jd_45a6b5953b15b", "zooooo58", "jd_66f5cecc1efcd", "jd_41345a6f96aa5"]
+let friendsArr = ["jd_5dae043c1f610", "18601623180_p", "tristan111", "jd_45a6b5953b15b", "zooooo58", "jd_66f5cecc1efcd", "jd_41345a6f96aa5"]
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
