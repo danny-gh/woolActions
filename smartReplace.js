2c60df35d60eb09f8a9088dd665cc8e054ba36d4
oe <oe@se.com>
2021-02-26 23:07:39 +0800

    jxnc token

--- smartReplace.js
+++ smartReplace.js
@@ -260,6 +260,13 @@ async function downloader_jd() {
             "USER_AGENTS"
         );
     }
+    if (remoteContent.indexOf("new Env('京喜财富岛')") > 0) {
+        await download(
+            "https://github.com/danny-gh/woolActions/raw/main/scripts/jdJxncTokens.js",
+            "./jdJxncTokens.js",
+            "京喜农场Token"
+        );
+    }
 }
 
 async function downloader_notify() {
