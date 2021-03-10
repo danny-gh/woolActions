7a0b39c97f72c4ee3afbb7a0608c3210045782c6
haifeng <haifeng@se.com>
2021-03-10 14:06:27 +0800

    test jx token

--- scripts/jdJxncTokens.js
+++ scripts/jdJxncTokens.js
@@ -6,7 +6,7 @@
 // 每个账号 token 是一个 json，示例如下
 // {"farm_jstoken":"749a90f871adsfads8ffda7bf3b1576760","timestamp":"1610165423873","phoneid":"42c7e3dadfadsfdsaac-18f0e4f4a0cf"}
 let JxncTokens = [
-  '',//账号一的京喜农场token
+  '{"farm_jstoken":"0fdd4ed41c7629c413d84877ed8bc4e1","timestamp":"1615355672070","phoneid":"c6514fa9843dc413"}',//账号一的京喜农场token
   '',//账号二的京喜农场token
 ]
 // 判断github action里面是否有京喜农场 token 
